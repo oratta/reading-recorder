de9f18cbc47b1e378207c84fb1820ce084c7830d
oratta <oratta@gmail.com>
2024-07-23 21:27:24 +0900

    Add Search page

--- src/lib/getter.js
+++ src/lib/getter.js
@@ -7,3 +7,29 @@ export async function getAllReviews() {
         }
     });
 }
+
+export function createBook(book){
+    const authors = book.volumeInfo.authors;
+    const price = book.saleInfo.listPrice;
+    const img = book.volumeInfo.imageLinks;
+    return {
+        id: book.id,
+        title: book.volumeInfo.title,
+        author: authors ? authors.join(',') : '',
+        price: price ? price.amount : 0,
+        publisher: book.volumeInfo.publisher,
+        published: book.volumeInfo.publishedDate,
+        image: img ? img.smallThumbnail : '/vercel.svg',
+    };
+}
+
+export async function getBooksByKeyword(keyword) {
+    const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${keyword}&langRestrict=ja&maxResults=20&printType=books`);
+    const result = await res.json();
+    const books = [];
+
+    for (const b of result .items) {
+        books.push(createBook(b));
+    }
+    return books;
+}
